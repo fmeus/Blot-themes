aafc5e7781a6fe0f684b3ed017b5c7bf2a761c1a
fmeus <frank.meus@gmail.com>
2015-09-10 13:55:22 +0200

    Redesign of archives page

--- SMD/Assets/scripts.js
+++ SMD/Assets/scripts.js
@@ -4,12 +4,24 @@
 
 /* Processing of code blocks for syntax highlighting and/or line numbers */
 $('pp').each(
-    function(){
-        var val = $(this).text();
-        if ( val ) {
+	function(){
+		var val = $(this).text();
+		if ( val ) {
 			$(this).next('pre').addClass('prettyprint linenums:'+val);
-        } else {          
+		} else {
 			$(this).next('pre').addClass('prettyprint');
-        }
-    }
+		}
+	}
+);
+
+// Handling of 'dividers' for archive page
+var smd_divider = "";
+$('h2.archive').each(
+	function(){
+		if ( smd_divider != $(this).text() ) {
+			smd_divider = $(this).text();
+		} else {
+			$(this).toggle();
+		}
+	}
 );
