c8093aa3c4984a0345da578cba5d4e96694c1915
fmeus <frank.meus@gmail.com>
2015-09-10 10:19:15 +0200

    Added comments

--- SMD/Assets/scripts.js
+++ SMD/Assets/scripts.js
@@ -2,6 +2,7 @@
 
 // This script is embedded in the footer of every page
 
+/* Processing of code blocks for syntax highlighting and/or line numbers */
 $('pp').each(
     function(){
         var val = $(this).text();
