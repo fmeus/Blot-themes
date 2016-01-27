d0c2d7050747944a3155349b452e3e19c7e6e7e7
fmeus <frank.meus@gmail.com>
2016-01-27 12:12:48 +0100

    Initial commit (using same structure as Blot local template)

---
+++ shared-memory-dump/script.js
@@ -0,0 +1,33 @@
+{{{appJS}}}
+
+// This script is embedded in the footer of every page
+
+// Processing of code blocks for syntax highlighting and/or line numbers
+$('pp').each(
+	function(){
+		var val = $(this).text();
+		if ( val ) {
+			$(this).next('pre').addClass('prettyprint linenums:'+val);
+		} else {
+			$(this).next('pre').addClass('prettyprint');
+		}
+	}
+);
+
+// Add dropshadow to images ending with `-ds`
+$('img').each(function() {
+	var filename = $(this).attr('src');
+	var imgName = filename.substr(0, filename.lastIndexOf('.')) || filename;
+
+	if ( imgName.substr(imgName.length - 3) == '-ds' ) {
+		$(this).addClass('dropshadow');
+	}
+});
+
+// Update title attribtue using alt attribute if not already set
+$('img').each(function() {
+	var title = $(this).prop('title');
+	if ( title === '' ) {
+		$(this).prop('title', $(this).prop('alt'));
+	}
+});
